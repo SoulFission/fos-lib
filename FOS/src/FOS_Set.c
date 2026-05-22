c1924160f7e9a1aa46d7d27d6d1a947599eee2ee
SoulFission <soulfission09@hotmail.com>
2026-05-23 04:33:28 +0800

    Fix some issues with FOS_Set

--- FOS/src/FOS_Set.c
+++ FOS/src/FOS_Set.c
@@ -24,10 +24,10 @@ bool FOS_set_contains(const FOS_Set *set, const void *elem)
 bool FOS_set_add(FOS_Set *set, const void *elem)
 {
     if (set == NULL || elem == NULL)
-        return 0;
+        return false;
 
     if (FOS_set_contains(set, elem))
-        return 0;
+        return false;
 
     // double lookup is required with the current implementation
     return FOS_hashmap_put(&set->hash_map, elem, &FOS_UNUSED);
@@ -51,6 +51,9 @@ size_t FOS_set_get_size(const FOS_Set *set)
 
 void FOS_set_iterate(const FOS_Set *set, void (*callback)(const void *elem, void *user), void *user)
 {
+    if (set == NULL || callback == NULL)
+        return;
+
     for (size_t i = 0; i < set->hash_map.capacity; ++i)
     {
         FOS_HashSlot *slot = &set->hash_map.slots[i];
